2b6260135889e93f89fc6760ed11a3e65b11339c
IshmamD <ishmam2001@outlook.com>
2024-12-02 23:58:48 -0500

    Update IRDP3.py

--- IRDP3.py
+++ IRDP3.py
@@ -11,6 +11,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 img1 = cv.imread('./data/motherboard_image.JPEG', cv.IMREAD_GRAYSCALE)
+th_img1 = cv.adaptiveThreshold(img1,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+edges = cv.Canny(th_img1,100,200)
 
+plt.figure(figsize=(10, 5))
+plt.subplot(121), plt.imshow(th_img1, cmap='gray')
+plt.title('Adaptive Thresholding'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(edges, cmap='gray')
+plt.title('Canny Edges'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+contours, hierarchy = cv.findContours(th_img1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+filtered_contours = []
+
+for cnt in contours:
+    if cv.contourArea(cnt) > 5000:  # Check if the contour's area is greater than 5000
+        filtered_contours.append(cnt)
