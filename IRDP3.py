5176e838546d9d41fb8af7c6c143a2a74908ff50
IshmamD <ishmam2001@outlook.com>
2024-12-04 18:23:16 -0500

    Update IRDP3.py

--- IRDP3.py
+++ IRDP3.py
@@ -10,10 +10,10 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+og_img1 = cv.imread('./data/motherboard_image.JPEG')
 img1 = cv.imread('./data/motherboard_image.JPEG', cv.IMREAD_GRAYSCALE)
-th_img1 = cv.adaptiveThreshold(img1,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
-edges = cv.Canny(th_img1,100,200)
+_, th_img1 = cv.threshold(img1, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+edges = cv.Canny(th_img1,1,200)
 
 plt.figure(figsize=(10, 5))
 plt.subplot(121), plt.imshow(th_img1, cmap='gray')
@@ -22,7 +22,9 @@ plt.subplot(122), plt.imshow(edges, cmap='gray')
 plt.title('Canny Edges'), plt.xticks([]), plt.yticks([])
 plt.show()
 
-contours, hierarchy = cv.findContours(th_img1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(th_img1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+#largest_contour = max(contours, key=cv.contourArea)
 
 filtered_contours = []
 
@@ -41,9 +43,10 @@ plt.show()
 
 cv.imwrite('./data/pcb_mask.png', mask)
 
-newimg1 = cv.bitwise_and(img1, img1, mask=mask)
+newimg1 = cv.bitwise_and(og_img1, og_img1, mask=mask)
+newimg1 = cv.cvtColor(newimg1, cv.COLOR_BGR2RGB)
 
-plt.figure(figsize=(5, 5))
+plt.figure(figsize=(15, 15))
 plt.imshow(newimg1)
 plt.title('Extracted Motherboard Image')
 plt.xticks([]), plt.yticks([])
